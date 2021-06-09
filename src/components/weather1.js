f44682c9939f88cf56b732c560e07b74bc674dca
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2021-06-09 12:33:09 +0530

    Create weather1.js

--- src/components/weather1.js
+++ src/components/weather1.js
@@ -1,17 +1,14 @@
 import React from 'react'
-import { Col, Row } from 'react-bootstrap'
 import WeatherCard from './weather2'
 import './weather.css';
 const WeatherList = ({weathers}) => {
     return (
       <div className="f">
-        <Row>
            {weathers.map(({dt,main, weather}) => (
-                <Col key={dt}>
+                <div key={dt}>
                     <WeatherCard temp_max={main.temp_max} temp_min={main.temp_min} dt={dt * 1000} main={weather[0].main} icon={weather[0].icon}/>
-                </Col>
+                </div>
             ))} 
-        </Row>
         </div>
     )
 }
