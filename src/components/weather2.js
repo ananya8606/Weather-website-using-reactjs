4ea4e616d9c1744b04b263f236e4833490f43750
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2021-06-09 13:32:33 +0530

    Create weather2.js

--- src/components/weather2.js
+++ src/components/weather2.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './weather.css';
 import './DisplayWeather.css';
-const WeatherCard = ({dt, temp_min, temp_max, main, icon}) => {
+const WeatherCard = ({dt,temp_min, temp_max, main, icon}) => {
   // create a date object with Date class constructor
   const date = new Date(dt);
   return (
 <div className="displayweather">
    <div className="maincard1">
-<img className="weather-icon" src={'http://openweathermap.org/img/wn/${icon}@2x.png'} alt="" srcset="" />
+<img className="weather-icon1" 
+        src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="" srcset="" />
 <span className="cardtitle">
               {main} 
             </span>
