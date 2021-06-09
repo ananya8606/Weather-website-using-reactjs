c2a2922e2fc916e6f3fd45ea053f6c85feecde47
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2021-06-09 12:33:54 +0530

    Create weather2.js

--- src/components/weather2.js
+++ src/components/weather2.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Card} from 'react-bootstrap';
 import './weather.css';
 import './DisplayWeather.css';
 const WeatherCard = ({dt, temp_min, temp_max, main, icon}) => {
@@ -8,24 +7,17 @@ const WeatherCard = ({dt, temp_min, temp_max, main, icon}) => {
   return (
 <div className="displayweather">
    <div className="maincard1">
-    <Card>
-      <Card.Img
-        variant="top"
-        // get the src from example url and pass the icon prop for icon code
-        src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
-      />
-      <Card.Body>
-        <Card.Title>{main}</Card.Title>
-        {/*  datetime is received in milliseconds, let's turn into local date time */}
+    <React.Fragment>
+<img className="weather-icon" src={'http://openweathermap.org/img/wn/${icon}@2x.png'} alt="" srcset="" />
+<span className="cardtitle">
+              {main} 
+            </span>
         <h5>
           {date.toLocaleDateString()} - {date.toLocaleTimeString()}
        </h5>
-        {/* minimum temperature */}
         <h5>Min: {temp_min}</h5>
-        {/* maximum temperature */}
         <h5>Max: {temp_max}</h5>
-      </Card.Body>
-    </Card>
+    </React.Fragment>
     </div>
     </div>
   );
